ebe91d42c197fa1cc94ce8b5a748018fb0edd1a2
Denis Korkhov <deniskorkhov@gmail.com>
2014-02-24 07:46:54 -0800

    Container Refactoring

--- CortoxaFramework/Cortoxa.Windsor/Service/ServiceBuilder.cs
+++ CortoxaFramework/Cortoxa.Windsor/Service/ServiceBuilder.cs
@@ -5,7 +5,7 @@
 //  * (LGPL) which accompanies this distribution, and is available at
 //  * http://www.gnu.org/licenses/lgpl.html
 //  *
-//  *  Filename:	ServiceRegistration.cs
+//  *  Filename:	ServiceBuilder.cs
 //  *  Date:		21/02/2014
 //  *  Author:   	Denis Korkhov
 //  *
@@ -18,27 +18,29 @@ using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 using Cortoxa.IoC;
 using Cortoxa.IoC2;
+using Cortoxa.IoC2.Base;
+using Cortoxa.IoC2.Service;
 using Cortoxa.Windsor.Interceptions;
 
 namespace Cortoxa.Windsor.Service
 {
-    internal class ServiceRegistration : IServiceRegistration
+    internal class ServiceBuilder : IServiceBuilder
     {
 //        private readonly IToolContainer2 container;
         private readonly RegistrationContext context;
 
-        private ServiceRegistration(RegistrationContext context)
+        private ServiceBuilder(RegistrationContext context)
         {
 //            this.container = container;
             this.context = context;
         }
 
-        public static IServiceRegistration For(Type type, IToolContainer2 container)
-        {
-            var context = new RegistrationContext(); ;
-            context.For = type;
-            return new ServiceRegistration(context);
-        }
+//        public static IServiceBuilder For(Type type, IToolContainer2 container)
+//        {
+////            var context = new RegistrationContext();
+////            context.For = type;
+//            return new ServiceBuilder(context);
+//        }
 
         /*public IToolContainer2 Done()
         {
@@ -95,29 +97,36 @@ namespace Cortoxa.Windsor.Service
         */
         public IServiceInterception Intercept { get; private set; }
 
-        public IServiceDependency Depend { get; private set; }
+//        public IServiceDependency Depend { get; private set; }
 
-        public IServiceRegistration To<T>()
+        public IServiceBuilder To<T>()
         {
             return To(typeof (T));
         }
 
-        public IServiceRegistration To(Type type)
+        public IServiceBuilder To(Type type)
         {
             context.To = type;
             return this;
         }
 
-        public IServiceRegistration ToSelf()
+        public IServiceBuilder ToSelf()
         {
             context.To = context.For;
             return this;
         }
 
-        public IServiceRegistration Name(string name)
+        public IServiceBuilder Name(string name)
         {
             context.Name = name;
             return this;
+        }
+
+        public RegistrationContext Context { get; private set; }
+
+        public IToolContainer2 Done()
+        {
+            throw new NotImplementedException();
         }
     }
 }
