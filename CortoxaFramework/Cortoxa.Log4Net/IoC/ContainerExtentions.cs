1ca05cea28ec843d48ad305a4b0e2e36d84ec409
Denis Korkhov <deniskorkhov@gmail.com>
2014-02-25 21:58:27 -0800

    Container Refactoring

--- CortoxaFramework/Cortoxa.Log4Net/IoC/ContainerExtentions.cs
+++ CortoxaFramework/Cortoxa.Log4Net/IoC/ContainerExtentions.cs
@@ -12,9 +12,7 @@
 //  */
 #endregion
 using System;
-using Cortoxa.Common.Log;
 using Cortoxa.IoC;
-using Cortoxa.IoC.Registration.Extentions;
 using log4net;
 
 namespace Cortoxa.Log4Net.IoC
@@ -24,15 +22,15 @@ namespace Cortoxa.Log4Net.IoC
         public static IToolContainer InstallLog4Net(this IToolContainer container, string configFile, string loggerName)
         {
 
-            container.Register(r => r.For<ILogger>()
-                .InterceptMethod<ILogger>(t => t.Debug(""), (c, o) => { GetLogger(loggerName).Debug(o.Arguments[0] as string); return null; })
-                .InterceptMethod<ILogger>(t => t.Info(""), (c, o) => { GetLogger(loggerName).Info(o.Arguments[0] as string); return null; })
-                .InterceptMethod<ILogger>(t => t.Info("", null), (c, o) => { GetLogger(loggerName).Info(string.Format((string)o.Arguments[0], o.Arguments[0])); return null; })
-                .InterceptMethod<ILogger>(t => t.Error(""), (c, o) => { GetLogger(loggerName).Error(o.Arguments[0] as string); return null; })
-                .InterceptMethod<ILogger>(t => t.Error("", default(Exception)), (c, o) => { GetLogger(loggerName).Error(o.Arguments[0] as string, o.Arguments[1] as Exception); return null; })
-                .InterceptMethod<ILogger>(t => t.Trace(""), (c, o) => { GetLogger(loggerName).Debug(o.Arguments[0] as string); return null; })
-                .InterceptMethod<ILogger>(t => t.Warn(""), (c, o) => { GetLogger(loggerName).Warn(o.Arguments[0] as string); return null; })
-                );
+//            container.Register(r => r.For<ILogger>()
+//                .InterceptMethod<ILogger>(t => t.Debug(""), (c, o) => { GetLogger(loggerName).Debug(o.Arguments[0] as string); return null; })
+//                .InterceptMethod<ILogger>(t => t.Info(""), (c, o) => { GetLogger(loggerName).Info(o.Arguments[0] as string); return null; })
+//                .InterceptMethod<ILogger>(t => t.Info("", null), (c, o) => { GetLogger(loggerName).Info(string.Format((string)o.Arguments[0], o.Arguments[0])); return null; })
+//                .InterceptMethod<ILogger>(t => t.Error(""), (c, o) => { GetLogger(loggerName).Error(o.Arguments[0] as string); return null; })
+//                .InterceptMethod<ILogger>(t => t.Error("", default(Exception)), (c, o) => { GetLogger(loggerName).Error(o.Arguments[0] as string, o.Arguments[1] as Exception); return null; })
+//                .InterceptMethod<ILogger>(t => t.Trace(""), (c, o) => { GetLogger(loggerName).Debug(o.Arguments[0] as string); return null; })
+//                .InterceptMethod<ILogger>(t => t.Warn(""), (c, o) => { GetLogger(loggerName).Warn(o.Arguments[0] as string); return null; })
+//                );
             return container;
         }
 
