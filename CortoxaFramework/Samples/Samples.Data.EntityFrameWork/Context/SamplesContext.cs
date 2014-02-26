6509cc0e2d6ff4a90cdae5c11c709bfd17a7d51b
Denis Korkhov <deniskorkhov@gmail.com>
2014-02-25 22:09:57 -0800

    Container Refactoring

---
+++ CortoxaFramework/Samples/Samples.Data.EntityFrameWork/Context/SamplesContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using Samples.Data.Entities;
+
+namespace Samples.Data.EntityFramework.Context
+{
+    public class SamplesContext : DbContext
+    {
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
