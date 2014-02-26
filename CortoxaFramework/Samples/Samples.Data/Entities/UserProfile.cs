69fa9700ba0a53bc50514bba02cc44c669779cad
Denis Korkhov <deniskorkhov@gmail.com>
2014-02-25 21:59:33 -0800

    Container Refactoring

---
+++ CortoxaFramework/Samples/Samples.Data/Entities/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Cortoxa.Data.Identity.Entitites;
+using Cortoxa.Data.Model;
+
+namespace Samples.Data.Entities
+{
+    public class UserProfile : Entity
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
