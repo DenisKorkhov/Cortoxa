3ed1c6d36c2d73c562ba6933e1b9e36b7737092c
Denis Korkhov <deniskorkhov@gmail.com>
2014-03-12 17:54:30 +0300

    Moved Identity package

---
+++ CortoxaFramework/Cortoxa.Data.Identity/Entitites/IdentityUser.cs
@@ -0,0 +1,49 @@
+﻿#region Copyright © 2014 Denis Korkhov, Oxagile (http://www.oxagile.com/)
+// /*
+//  * All rights reserved. This program and the accompanying materials
+//  * are made available under the terms of the GNU Lesser General Public License
+//  * (LGPL) version 2.1 which accompanies this distribution, and is available at
+//  * http://www.gnu.org/licenses/lgpl-2.1.html
+//  *
+//  *  Filename:	IdentityUser.cs
+//  *  Date:		10/02/2014
+//  *  Author:   	Denis Korkhov
+//  *
+//  */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using Cortoxa.Data.Model;
+using Microsoft.AspNet.Identity;
+
+namespace Cortoxa.Data.Identity.Entitites
+{
+    public class IdentityUser : Entity, IUser
+    {
+        string IUser.Id
+        {
+            get { return Id.ToString(); }
+        }
+
+        public virtual string UserName { get; set; }
+
+        public virtual string PasswordHash { get; set; }
+
+        public virtual string SecurityStamp { get; set; }
+
+        public virtual ICollection<IdentityRole> Roles { get; set; }
+
+        public virtual ICollection<IdentityUserClaim> Claims { get; set; }
+
+        public IdentityUser() 
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public IdentityUser(string userName) : this()
+        {
+            UserName = userName;
+        }
+    }
+}
---
+++ CortoxaFramework/Cortoxa.Data.Identity/Entitites/IdentityUserRole.cs
@@ -0,0 +1,27 @@
+﻿#region Copyright © 2014 Denis Korkhov, Oxagile (http://www.oxagile.com/)
+// /*
+//  * All rights reserved. This program and the accompanying materials
+//  * are made available under the terms of the GNU Lesser General Public License
+//  * (LGPL) version 2.1 which accompanies this distribution, and is available at
+//  * http://www.gnu.org/licenses/lgpl-2.1.html
+//  *
+//  *  Filename:	IdentityUserRole.cs
+//  *  Date:		10/02/2014
+//  *  Author:   	Denis Korkhov
+//  *
+//  */
+#endregion
+
+namespace Cortoxa.Data.Identity.Entitites
+{
+    public class IdentityUserRole
+    {
+        public virtual string UserId { get; set; }
+
+        public virtual string RoleId { get; set; }
+
+        public virtual IdentityRole Role { get; set; }
+
+        public virtual IdentityUser User { get; set; } 
+    }
+}
