f9c70ff2712dac15ee3efeb46eebe40a8d82d0f9
Denis Korkhov <deniskorkhov@gmail.com>
2014-04-09 16:22:27 +0300

    Log4Net implementation

--- CortoxaFramework/Cortoxa.Log4Net/Log4NetSetup.cs
+++ CortoxaFramework/Cortoxa.Log4Net/Log4NetSetup.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Cortoxa.Common.Log;
+using Cortoxa.Configuration;
 using Cortoxa.Container.Components;
+using Cortoxa.Container.Extentions;
+using Cortoxa.Container.Registrator;
 using Cortoxa.Container.Services;
 using log4net;
 
@@ -8,20 +11,21 @@ namespace Cortoxa.Log4Net
 {
     public static class Log4NetSetup
     {
-        public static IComponentConfigurator<LoggerComponent> Nlog(this IComponentRegistrator componentRegistrator)
+        public static void Log4Net(this IConfigurator<IRegistrator> registrator)
         {
-            return componentRegistrator.Configure<LoggerComponent>(c => c.Logger, r => r.For<ILogger>()
+            registrator.Configure(r=>r.For<ILogger>(c=>c
                 .Intercept.Method<ILogger>(m => m.Info(default(string)), context => GetLogger(context.ResolverType.FullName).Info(context.Arguments[0] as string))
                 .Intercept.Method<ILogger>(m => m.Trace(default(string)), context => GetLogger(context.ResolverType.FullName).Info(context.Arguments[0] as string))
-                .Intercept.Method<ILogger>(m => m.Warn(default(string)), context => GetLogger(context.ResolverType.FullName).Info(context.Arguments[0] as string))
-                .Intercept.Method<ILogger>(m => m.Debug(default(string)), context => GetLogger(context.ResolverType.FullName).Info(context.Arguments[0] as string))
-                .Intercept.Method<ILogger>(m => m.Error(default(string)), context => GetLogger(context.ResolverType.FullName).Info(context.Arguments[0] as string))
+                .Intercept.Method<ILogger>(m => m.Warn(default(string)), context => GetLogger(context.ResolverType.FullName).Warn(context.Arguments[0] as string))
+                .Intercept.Method<ILogger>(m => m.Debug(default(string)), context => GetLogger(context.ResolverType.FullName).Debug(context.Arguments[0] as string))
+                .Intercept.Method<ILogger>(m => m.Error(default(string)), context => GetLogger(context.ResolverType.FullName).Error(context.Arguments[0] as string))
                 .Intercept.Method<ILogger>(m => m.Info(default(string), default(string[])), context => GetLogger(context.ResolverType.FullName).Info(context.Arguments[0] as string))
-                .Intercept.Method<ILogger>(m => m.Trace(default(string), default(string[])), context => GetLogger(context.ResolverType.FullName).Debug(context.Arguments[0] as string))
+                .Intercept.Method<ILogger>(m => m.Trace(default(string), default(string[])), context => GetLogger(context.ResolverType.FullName).Info(context.Arguments[0] as string))
                 .Intercept.Method<ILogger>(m => m.Warn(default(string), default(string[])), context => GetLogger(context.ResolverType.FullName).Warn(context.Arguments[0] as string))
                 .Intercept.Method<ILogger>(m => m.Debug(default(string), default(string[])), context => GetLogger(context.ResolverType.FullName).Debug(context.Arguments[0] as string))
                 .Intercept.Method<ILogger>(m => m.Error(default(string), default(string[])), context => GetLogger(context.ResolverType.FullName).Error(context.Arguments[0] as string))
-                .Intercept.Method<ILogger>(m => m.Error(default(string), default(Exception)), context => GetLogger(context.ResolverType.FullName).Error(context.Arguments[0] as string, context.Arguments[1] as Exception)));
+                .Intercept.Method<ILogger>(m => m.Error(default(string), default(Exception)), context => GetLogger(context.ResolverType.FullName).Error(context.Arguments[0] as string, context.Arguments[1] as Exception))
+            ));
         }
         private static ILog GetLogger(string name)
         {
